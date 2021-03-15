e1803220d643018d62f148970fdac202f52b7038
Ingrid Inara <ingridinara@me.com>
2021-03-15 09:27:57 +0100

    featuring header jumbo tron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Photos from './components/Photos';
 import HeaderNavBar from './components/composed/HeaderNavBar';
+import HeaderJumbotron from './components/composed/HeaderJumbotron';
 
 function App() {
   return (
     <>
       <HeaderNavBar />
+      <HeaderJumbotron />
       <Photos />
     </>
   );
---
+++ src/components/composed/HeaderJumbotron.styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import image from 'assets/image.jpg';
+export const StyledJumbotron = styled.div`
+  .jumbo {
+    background: url(${image}) no-repeat fixed bottom;
+    background-size: cover;
+    color: #efefef;
+    height: 50vh;
+    position: relative;
+    z-index: -2;
+  }
+
+  .overlay {
+    background-color: #000;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+`;
+
+export const StyledTitle = styled.h1`
+  color: green;
+  text-align: center;
+`;
+
+export const StyledParagraphDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 1rem;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 1rem;
+`;
+
+export const StyledSubTitle = styled.h4`
+  color: green;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
